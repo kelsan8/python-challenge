6bf8dd53b693c54b19abdf6e6206465cd2107aed
kelsan8 <46331171+kelsan8@users.noreply.github.com>
2019-02-03 16:23:52 -0800

    Finalized code

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,7 +3,8 @@ import os
 import csv
 
 # Set the file path for the data set
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+#csvpath = os.path.join("..", "Resources", "budget_data.csv")
+csvpath = os.path.join("C:/Users/keber/Desktop/Data Science Boot Camp/Resources/budget_data.csv")
 
 # Set up variables to track data
 num_months = 1
@@ -18,20 +19,25 @@ last_revenue = 0
 with open(csvpath, newline = "") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter = ",")
 
-    row_revenue = float(row[1])
+    # Skips the header row
+    header = next(csv_reader, None)
+
+    row = next(csv_reader, None)
+
+    row_revenue = int(row[1])
+    last_revenue = int(row[1])
     greatest_inc_month = row[0]
     greatest_inc_revenue = row_revenue
     greatest_dec_month = row[0]
     greatest_dec_revenue = row_revenue
 
-    # Skips the header row
-    header = next(csv_reader, None)
+    
 
     # Cycle through each row in the data file to grab data
     for row in csv_reader:
 
         # Grabs the revenue value for the row of the iteration
-        # row_revenue = long(row[1])
+        row_revenue = int(row[1])
 
         # Increments the net_revenue by the revenue of the current row
         net_revenue = net_revenue + row_revenue
@@ -57,27 +63,28 @@ with open(csvpath, newline = "") as csvfile:
         last_revenue = float(row[1])
 
     # Calculate the average revenue and average change in revenue for the data
-    avg_revenue = net_revenue / num_months
+    # avg_revenue = net_revenue / num_months
     avg_revenue_change = avg_revenue_change / num_months
 
 # Print the results to the terminal
 print("Financial Analysis")
 print("----------------------------")
 print(f'Total Months: {num_months}')
-print(f'Total Revenue: {net_revenue}')
-print(f'Average Change: {avg_revenue_change}')
-print(f'Greatest Increase in Profits: {greatest_inc_month} {greatest_inc_revenue}')
-print(f'Greatest Decrease in Profits: {greatest_dec_month} {greatest_dec_revenue}')
+print(f'Total Revenue: ${net_revenue}')
+print(f'Average Change: ${avg_revenue_change}')
+print(f'Greatest Increase in Profits: {greatest_inc_month} ${greatest_inc_revenue}')
+print(f'Greatest Decrease in Profits: {greatest_dec_month} ${greatest_dec_revenue}')
 
 # Create a new .txt file to print the results to
-output_path = os.path.join("..", "output", "mainoutput.txt")
+# output_path = os.path.join("..", "output", "mainoutput.txt")
+output_path = os.path.join("C:/Users/keber/Desktop/Data Science Boot Camp/Resources/pybank_output.txt")
 
 # Opens the newly made file in write mode and writes the results to it
 with open(output_path, 'w', newline = '') as writefile:
     writefile.writelines("Financial Analysis \n")
     writefile.writelines("---------------------------- \n")
     writefile.writelines(f'Total Months: {num_months} \n')
-    writefile.writelines(f'Total Revenue: {net_revenue} \n')
-    writefile.writelines(f'Average Change: {avg_revenue_change} \n')
-    writefile.writelines(f'Greatest Increase in Profits: {greatest_inc_month} {greatest_inc_revenue} \n')
-    writefile.writelines(f'Greatest Decrease in Profits: {greatest_dec_month} {greatest_dec_revenue} \n')
+    writefile.writelines(f'Total Revenue: ${net_revenue} \n')
+    writefile.writelines(f'Average Change: ${avg_revenue_change} \n')
+    writefile.writelines(f'Greatest Increase in Profits: {greatest_inc_month} ${greatest_inc_revenue} \n')
+    writefile.writelines(f'Greatest Decrease in Profits: {greatest_dec_month} ${greatest_dec_revenue} \n')
