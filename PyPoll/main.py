521d34bace7c51bfd72df94d2c685a403be98d9b
kelsan8 <46331171+kelsan8@users.noreply.github.com>
2019-02-05 15:33:25 -0800

    Final commentary added

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -26,55 +26,49 @@ with open(csvpath, newline = "") as csvfile:
     # Sets the row to read data from
     row = next(csv_reader, None)
 
+    # Cycles through all of the rows (except the header) in the file
     for row in csv_reader:
 
-        # candidates.append(row[2])
-
-        # num_votes_candidate.append(0)
-
-        # vote_percentage.append(0)
-
+        # Sets the initial candidate name to the first value read in column 3
         candidate_name = row[2]
 
+        # Increments the total votes accumulator by 1 for each row
         total_votes = total_votes + 1
 
-        #if candidate_name == row[2]:
-            # Set an accumulator for the if statement
-            #x = 1
-            # Add x to the candidate votes accumulator
-            #candidate_votes = x
-
-        # if row[2] = candidate_name: 
-        # if candidate in candidates: ...?
-
-        #if row[2] in candidates_list.keys():
-            #candidates_list(row[2]) = candidates_list(row[2]) + 1
-
-        #else:
-            #candidates_list(row[2]) = 1
-
+        # If the value in candidate_name is found in the candidates list
+            # the candidate number will be set to the index number of the candidate in question
+            # Then the corresponding num_votes_list index number will increment by 1
+            # Same with the vote_percentage index number for the corresponding candidate index
         if candidate_name in candidates:
             candidate_num = candidates.index(candidate_name)
             num_votes_candidate[candidate_num] = num_votes_candidate[candidate_num] + 1
             vote_percentage[candidate_num] = vote_percentage[candidate_num] + 1
+            # Nested if statement that will compare the number of votes per candidate with the value in winning_votes
+                # If it's higher, the winning vote is set to the amount of that candidate and 
+                # will set the value of election_winner to the name of that candidate
             if num_votes_candidate[candidate_num] > winning_votes:
                 election_winner = candidate_name
                 winning_votes = num_votes_candidate[candidate_num]
                 election_winner = candidate_name
+        # Otherwise, the new candidate name will be added to the candidates list along with number of votes and vote percentage
         else:
             candidates.append(row[2])
             num_votes_candidate.append(1)
             vote_percentage.append(1)
 
+    # Counts the number of candidates based on the candidates list
     num_candidates = len(candidates)
 
+    # Creates a new list to keep the total percentage of votes each candidate received
     candidate_total_percent = []
+    # Loops through the number of candidates to calculate their percentage of votes and adds it to the 
+        # candidate_total_percent list
     for i in range(num_candidates):
         candidate_percent = (vote_percentage[i] / total_votes) * 100
         candidate_total_percent.append(candidate_percent)
 
     
-
+# Prints the election results to the terminal with a loop to print each candidate's results
 print("Election Results")
 print("-------------------------")
 print(f'Total Votes: {total_votes}')
@@ -85,8 +79,10 @@ print("-------------------------")
 print(f'Winner: {election_winner}')
 print("-------------------------")
 
+# Creates a new file to write the results to
 output_path = os.path.join("C:/Users/keber/Desktop/Data Science Boot Camp/python-challenge/pypoll_output.txt")
 
+# Opens the file in write mode to print the results, using the same loop to print each candidate's results
 with open(output_path, 'w') as writefile:
     writefile.writelines("Election Results \n")
     writefile.writelines("------------------------- \n")
